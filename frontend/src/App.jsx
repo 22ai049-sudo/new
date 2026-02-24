f0d80d527d79a52c8f9e61f0692ac3e10e6e1887
S.K.ABHYUDHY <22ai049@ssit.edu.in>
2026-02-24 13:20:27 +0530

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import AnalystDashboard from "./components/AnalystDashboard";
-import "./styles.css";
-
-export default function App() {
-  return <AnalystDashboard />;
-}
